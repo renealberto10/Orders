24c08a8af3f850c77ba1da063ebdc70a87ddc716
renealberto10 <renealberto10@gmail.com>
2020-07-11 12:02:35 -0600

    workin api heroku

--- app/src/main/java/com/pumpkinapplabs/orders/data/remote/ApiUtils.java
+++ app/src/main/java/com/pumpkinapplabs/orders/data/remote/ApiUtils.java
@@ -3,7 +3,7 @@ package com.pumpkinapplabs.orders.data.remote;
 public class ApiUtils {
     private ApiUtils() {}
 
-    public static final String BASE_URL = "http://192.168.0.6:8000/api/";
+    public static final String BASE_URL = "http://pumpkinapplab.herokuapp.com/api/";
 
     public static APIService getAPIService() {
 
