b5a3f0972177f6a7f131c072d8f4d407a95cca3e
renealberto10 <renealberto10@gmail.com>
2020-08-23 22:06:42 -0600

    update
    update

--- app/src/main/java/com/pumpkinapplabs/orders/InvetoryActivity.java
+++ app/src/main/java/com/pumpkinapplabs/orders/InvetoryActivity.java
@@ -4,39 +4,20 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaSession2Service;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.pumpkinapplabs.orders.data.model.ItemCustomer;
-import com.pumpkinapplabs.orders.data.model.ItemInventory;
-import com.pumpkinapplabs.orders.data.utils.Constants;
-import com.pumpkinapplabs.orders.data.utils.UtilCustomer;
-import com.pumpkinapplabs.orders.data.utils.UtilsInventory;
-import com.pumpkinapplabs.orders.ui.inventory.CustomerFragment;
-import com.pumpkinapplabs.orders.ui.inventory.InventoryFragment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-public class MenuActivity extends AppCompatActivity /*implements InventoryFragment.OnFragmentInteractionListener*/ {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-
+public class InvetoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-        //Constants.instance(this.getApplicationContext());
-        // UtilsInventory.getdatainventory();
-        // UtilCustomer.getdatacustomer();
-
+        setContentView(R.layout.activity_invetory);
 //Menu
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
@@ -44,31 +25,26 @@ public class MenuActivity extends AppCompatActivity /*implements InventoryFragme
             @Override
             public boolean onNavigationItemSelected (@NonNull MenuItem item){
                 Intent intent = null;
-
                 switch (item.getItemId()) {
                     case R.id.navigation_inventory:
-                        intent = new Intent(MenuActivity.this, InvetoryActivity.class);
+                        intent = new Intent(InvetoryActivity.this, InvetoryActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        return true;
+                       return true;
                     case R.id.navigation_inicio:
-                        intent = new Intent(MenuActivity.this, MenuActivity.class);
+                        intent = new Intent(InvetoryActivity.this, MenuActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         break;
-                      //  break;
                     case R.id.navigation_pedidos:
-                        intent = new Intent(MenuActivity.this, OrderActivity.class);
+                        intent = new Intent(InvetoryActivity.this, OrderActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                       break;
+                        break;
                     default:
-                        return false;
                 }
                 startActivity(intent);
-                return true;
+                return false;
             }
         });
-}
-
-
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -92,13 +68,4 @@ public class MenuActivity extends AppCompatActivity /*implements InventoryFragme
         startActivity(intent);
     }
 
-    //Este evento clic hereda del fragment Inventory
-   /* @Override
-    public void onListClick(ItemInventory inventory) {
-    }*/
-
-    //Este evento clic hereda del fragment Inventory
-  //  @Override
-  //  public void onListClick(ItemCustomer customer) {
-  //  }
 }
--- app/src/main/java/com/pumpkinapplabs/orders/OrderActivity.java
+++ app/src/main/java/com/pumpkinapplabs/orders/OrderActivity.java
@@ -1,41 +1,24 @@
 package com.pumpkinapplabs.orders;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaSession2Service;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.pumpkinapplabs.orders.data.model.ItemCustomer;
-import com.pumpkinapplabs.orders.data.model.ItemInventory;
-import com.pumpkinapplabs.orders.data.utils.Constants;
-import com.pumpkinapplabs.orders.data.utils.UtilCustomer;
-import com.pumpkinapplabs.orders.data.utils.UtilsInventory;
-import com.pumpkinapplabs.orders.ui.inventory.CustomerFragment;
-import com.pumpkinapplabs.orders.ui.inventory.InventoryFragment;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-public class MenuActivity extends AppCompatActivity /*implements InventoryFragment.OnFragmentInteractionListener*/ {
-
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-        //Constants.instance(this.getApplicationContext());
-        // UtilsInventory.getdatainventory();
-        // UtilCustomer.getdatacustomer();
+        setContentView(R.layout.activity_order);
 
 //Menu
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view);
@@ -44,30 +27,26 @@ public class MenuActivity extends AppCompatActivity /*implements InventoryFragme
             @Override
             public boolean onNavigationItemSelected (@NonNull MenuItem item){
                 Intent intent = null;
-
                 switch (item.getItemId()) {
                     case R.id.navigation_inventory:
-                        intent = new Intent(MenuActivity.this, InvetoryActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        return true;
-                    case R.id.navigation_inicio:
-                        intent = new Intent(MenuActivity.this, MenuActivity.class);
+                        intent = new Intent(OrderActivity.this, InvetoryActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         break;
-                      //  break;
-                    case R.id.navigation_pedidos:
-                        intent = new Intent(MenuActivity.this, OrderActivity.class);
+                    case R.id.navigation_inicio:
+                        intent = new Intent(OrderActivity.this, MenuActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                       break;
+                    break;
+                    case R.id.navigation_pedidos:
+                        intent = new Intent(OrderActivity.this, OrderActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                      return true;
                     default:
-                        return false;
                 }
                 startActivity(intent);
-                return true;
+                return false;
             }
         });
-}
-
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,13 +71,4 @@ public class MenuActivity extends AppCompatActivity /*implements InventoryFragme
         startActivity(intent);
     }
 
-    //Este evento clic hereda del fragment Inventory
-   /* @Override
-    public void onListClick(ItemInventory inventory) {
-    }*/
-
-    //Este evento clic hereda del fragment Inventory
-  //  @Override
-  //  public void onListClick(ItemCustomer customer) {
-  //  }
 }
